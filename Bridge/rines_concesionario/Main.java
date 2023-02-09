b33b215323533635cfd37d16ab57ec78df103788
Julianpulido2003 <98838164+Julianpulido2003@users.noreply.github.com>
2023-02-08 20:37:14 -0500

    Cambio final final

---
+++ Bridge/rines_concesionario/Main.java
@@ -0,0 +1,17 @@
+package rines_concesionario;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Forma rinAcero = new Rin(100, 100, 10, 17, new RinAcero());
+        Forma rinALuminio = new Rin(100, 100, 10, 18, new RinAluminio());
+        Forma rinAleacion = new Rin(100, 100, 10, 19, new RinAleacion());
+
+        rinAcero.elegir();
+        rinALuminio.elegir();
+        rinAleacion.elegir();
+
+
+    }
+}
